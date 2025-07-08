9bb7232f6c0c68821d25d4feb8da9811a0416c1e
Olga Oliveira <105463799+olguinha@users.noreply.github.com>
2025-07-08 13:10:31 -0400

    novos apredizados de variaveis
     number, bigint e string

---
+++ Modulo 1/AT6/STRG.js
@@ -0,0 +1,12 @@
+"use strict";
+// Exemplo 1 - Single Quotes
+let nomecompleto = 'Olga de Oliveira';
+console.log(nomecompleto);
+// Exemplo 2 - Double Quotes
+let funcaoempresa = "em engine software";
+console.log(funcaoempresa);
+// Exemplo 3 - Backticks (template string)
+let nomeEmpresa = 'Google';
+let dadosFuncionario = `Seja bem-vinda ${nomecompleto}! Você será ${funcaoempresa}
+na empresa ${nomeEmpresa}.`;
+console.log(dadosFuncionario);
---
+++ Modulo 1/AT6/STRG.ts
@@ -0,0 +1,16 @@
+// Exemplo 1 - Single Quotes
+let nomecompleto: string = 'Olga de Oliveira';
+console.log(nomecompleto);
+
+// Exemplo 2 - Double Quotes
+let funcaoempresa: string = "em engine software";
+console.log(funcaoempresa);
+
+// Exemplo 3 - Backticks (template string)
+let nomeEmpresa: string = 'Google';
+
+let dadosFuncionario: string = `Seja bem-vinda ${nomecompleto}! Você será ${funcaoempresa}
+na empresa ${nomeEmpresa}.`;
+
+console.log(dadosFuncionario);
+
