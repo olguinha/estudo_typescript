c96a4aea045bc246783e12a63c3c3fe1a88436f5
Olga Oliveira <105463799+olguinha@users.noreply.github.com>
2025-11-16 19:08:36 -0400

    tetetet
    rese

---
+++ Modulo 1/AT11/UNK.ts
@@ -0,0 +1,48 @@
+// exemplo 1 : TIPO UNKNOWN
+
+let valorvariavel: unknown;
+
+valorvariavel = true;
+valorvariavel = 123;
+valorvariavel = [];
+valorvariavel = 'Oi, tudo bem?';
+
+console.log(valorvariavel);
+
+
+// EXEMPLO 2: ERRO AO TENTAR ATRIBUIR UM VALOR DO TIPO 'UNKNOWN' A OUTROS TIPOS:
+
+// let valor: unknown;
+
+// let valor1: boolean = valor;
+// let valor2: any = valor;
+// let valor3: any[] = valor;
+// let valor4: string = valor;
+
+// EXEMPLO 3: DIFERENÇA ENTRE 'ANY' VS 'UNKNOWN'
+
+let algumacoisa: any;
+
+let algumascoisas: unknown;
+
+
+console.log(algumacoisa.toFixed(2));
+
+
+// ele sempre vai te forçar a fazer um if e else, já para não quebrar seu codigo, isso acaba te facilitando na hora de programar...
+
+
+if (typeof algumascoisas == 'number'){
+    console.log(algumascoisas.toFixed(2));
+}
+
+
+
+
+
+
+
+
+
+
+
